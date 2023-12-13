c8f27ad7315a92c3ca0cd8c4a8d5785a8e8ab5cf
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-12-13 11:14:44 -0300

    feat: altera styles e integra banco de dados

--- main/settings.py
+++ main/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from django.utils.module_loading import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
